263983bec8cba671abe3ec36ce271ef08401a649
hidrodo <hidrodo@gmail.com>
2014-04-17 16:06:25 +0700

    Update
    Đối tượng MainApp chứa inputString và outputString.
    Trong đó inputString tạm thời được hard code, có thể truyền nội dung vào
    bất cứ lúc nào.
    Kết quả trả về outputString.

--- JPFTest/src/MainApp.java
+++ JPFTest/src/MainApp.java
@@ -1,39 +1,129 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import gov.nasa.jpf.JPF;
 import gov.nasa.jpf.Config;
 
 public class MainApp {
-	private static String targetClass = "DiningPhil";
-	private static String classPath = ".\\bin";
-	private static String sourcePath = ".\\src";
-	private static String inputString;  
-	private static String options = "error,trace";
+	private String targetClass;
+	private String classPath;
+	private String sourcePath;
+	private String inputString;  
+	private String options;
 	private String outputString;
+	private String targetFile;
+	private String reportFile;
 	
-	public static void intoString(){
-		inputString = " public class className {" +
+	public String getTargetClass() {
+		return targetClass;
+	}
+	public void setTargetClass(String targetClass) {
+		this.targetClass = targetClass;
+	}
+	public String getClassPath() {
+		return classPath;
+	}
+	public void setClassPath(String classPath) {
+		this.classPath = classPath;
+	}
+	public String getSourcePath() {
+		return sourcePath;
+	}
+	public void setSourcePath(String sourcePath) {
+		this.sourcePath = sourcePath;
+	}
+	public String getInputString() {
+		return inputString;
+	}
+	public void setInputString(String inputString) {
+		this.inputString = inputString;
+	}
+	public String getOptions() {
+		return options;
+	}
+	public void setOptions(String options) {
+		this.options = options;
+	}
+	public String getOutputString() {
+		return outputString;
+	}
+	public void setOutputString(String outputString) {
+		this.outputString = outputString;
+	}
+	public String getTargetFile() {
+		return targetFile;
+	}
+	public void setTargetFile(String targetFile) {
+		this.targetFile = targetFile;
+	}
+	public String getReportFile() {
+		return reportFile;
+	}
+	public void setReportFile(String reportFile) {
+		this.reportFile = reportFile;
+	}
+	
+	public MainApp(){
+		// default constructor
+		setTargetClass("Target");
+		setClassPath(".\\bin");
+		setSourcePath(".\\src");
+		setOptions("error,trace");
+		setTargetFile(this.sourcePath + "\\" + this.targetClass + ".java");
+		setReportFile(this.sourcePath + "\\report"); 
+	}
+	
+	public void printToFile() throws FileNotFoundException{
+		PrintWriter writer = new PrintWriter(this.targetFile);
+		writer.println(this.inputString);
+		writer.close();
+	}
+	
+	public void setOutputString(String path, Charset encoding) throws IOException{
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		this.outputString = new String(encoded, encoding);
+	}
+	
+	public void deleteFile(String fileName){
+		File tempFile = new File(fileName);
+		tempFile.deleteOnExit();
+	}
+//========================================================================================
+	
+	public static void main(String[] args) throws IOException{
+		MainApp mainApp = new MainApp();
+		String inString = " public class Target {" +
 				"public static void main(String[] args){" +
 				"System.out.println(123);"+
 				"  }" +
 				"}";
-	}
-	public static void printToFile() throws FileNotFoundException{
-		PrintWriter writer = new PrintWriter(sourcePath + "\\" + targetClass+".java");
-		writer.println(inputString);
-		writer.close();
-	}
-	public static void main(String[] args) throws FileNotFoundException{
-		intoString();
-		//printToFile();
+		mainApp.setInputString(inString);
+		mainApp.printToFile();
 		
 		Config config = JPF.createConfig(args);
-		config.setProperty("target", targetClass);
-		config.setProperty("report.console.property_violation", options);
-		config.setProperty("classpath", classPath);
-		config.setProperty("sourcepath", sourcePath);
+		config.setProperty("target", mainApp.targetClass);
+		config.setProperty("report.console.start", "");
+		config.setProperty("report.console.finished", "");
+		config.setProperty("report.console.property_violation", mainApp.options);
+		config.setProperty("report.console.file", mainApp.reportFile);
+		config.setProperty("classpath", mainApp.classPath);
+		config.setProperty("sourcepath", mainApp.sourcePath);
+		
 		JPF jpf = new JPF(config);
+		
 		jpf.run();
+		
+		mainApp.setOutputString(mainApp.reportFile, Charset.defaultCharset());
+		
+		mainApp.deleteFile(mainApp.targetFile);
+		mainApp.deleteFile(mainApp.reportFile);
+		
+		//System.out.println(mainApp.outputString);
 	}
 
 }
