e40f06b22efc5c0003daa998e074f180484e5461
anhtrung93 <trunganhdinh@gmail.com>
2014-04-24 10:18:15 +0700

    Remove unnecessary files, Resolve conflicts

--- src/main/java/com/example/models/MetaData.java
+++ src/main/java/com/example/models/MetaData.java
@@ -1,160 +1,246 @@
 package com.example.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class MetaData {
-	private String Name;
-	private String DisplayName;
-	private String Version;
-	private String Email;
-	private String SupportEmail;
-	private String TermsOfUseUrl;
-	private String PrivacyUrl;
-	private String Institution;
-	private String InstitutionUrl;
-	private String InstitutionImageUrl;
-	private String MimeType;
-	private boolean SupportsLanguageSyntax;
-	private String Title;
-	private String Description;
-	private String Question;
-	private String Url;
-	private String VideoUrl;
-	private boolean DisableErrorTable;
-	
-	public MetaData(){
-		
+	@XmlElement(name = "Name")
+	private String name;
+	@XmlElement(name = "DisplayName")
+	private String displayName;
+	@XmlElement(name = "Version")
+	private String version;
+	@XmlElement(name = "Email")
+	private String email;
+	@XmlElement(name = "SupportEmail")
+	private String supportEmail;
+	@XmlElement(name = "TermsOfUseUrl")
+	private String termsOfUseUrl;
+	@XmlElement(name = "PrivacyUrl")
+	private String privacyUrl;
+	@XmlElement(name = "Institution")
+	private String institution;
+	@XmlElement(name = "InstitutionUrl")
+	private String institutionUrl;
+	@XmlElement(name = "InstitutionImageUrl")
+	private String institutionImageUrl;
+	@XmlElement(name = "MimeType")
+	private String mimeType;
+	@XmlElement(name = "SupportsLanguageSyntax")
+	private boolean supportsLanguageSyntax;
+	@XmlElement(name = "Title")
+	private String title;
+	@XmlElement(name = "Description")
+	private String description;
+	@XmlElement(name = "Question")
+	private String question;
+	@XmlElement(name = "Url")
+	private String url;
+	@XmlElement(name = "VideoUrl")
+	private String videoUrl;
+	@XmlElement(name = "DisableErrorTable")
+	private boolean disableErrorTable;
+	@XmlElement(name = "Samples")
+	private List<Sample> samples;
+	@XmlElement(name = "Tutorials")
+	private List<Tutorial> tutorials;
+
+	public MetaData() {
+
 	}
-	
-	public void init(){
-		this.Name = "echo";
-		this.DisplayName = "Echo";
-		this.Version = "1.0";
-		this.Email = "rise4fun-feedback@microsoft.com";
-		this.SupportEmail = "rise4fun-feedback@microsoft.com";
-		this.TermsOfUseUrl = "http://rise4fun.com/termsofuse";
-		this.PrivacyUrl = "http://rise4fun.com/privacy";
-		this.Institution = "Microsoft Research";
-		this.InstitutionUrl = "http://rise4fun.com";
-		this.InstitutionImageUrl = "http://rise4fun.com/images/logo_msr_small.png";
-		this.MimeType = "text/x-echo";
-		this.SupportsLanguageSyntax = true;
-		this.Title = "Echoes the text";
-		this.Description = "An example of remote tool that simplies returns the original text.";
-		this.Question = "Where are you, echo?";
-		this.Url = "http://rise4fun.com";
-		this.VideoUrl = "http://rise4fun.com/video";
-		this.DisableErrorTable = false;		
+
+	public void init() {
+		this.name = "echo";
+		this.displayName = "Echo";
+		this.version = "1.0";
+		this.email = "rise4fun-feedback@microsoft.com";
+		this.supportEmail = "rise4fun-feedback@microsoft.com";
+		this.termsOfUseUrl = "http://rise4fun.com/termsofuse";
+		this.privacyUrl = "http://rise4fun.com/privacy";
+		this.institution = "Microsoft Research";
+		this.institutionUrl = "http://rise4fun.com";
+		this.institutionImageUrl = "http://rise4fun.com/images/logo_msr_small.png";
+		this.mimeType = "text/x-echo";
+		this.supportsLanguageSyntax = false;
+		this.title = "Echoes the text";
+		this.description = "An example of remote tool that simplies returns the original text.";
+		this.question = "Where are you, echo?";
+		this.url = "http://rise4fun.com";
+		this.videoUrl = "http://rise4fun.com/video";
+		this.disableErrorTable = false;
+
+		this.samples = new ArrayList<Sample>();
+		this.samples.add(new Sample("hello", "hello world"));
+		this.samples.add(new Sample("bye", "see you"));
+
+		this.tutorials = new ArrayList<Tutorial>();
+		Tutorial newTutorial = new Tutorial();
+		newTutorial.init();
+		this.tutorials.add(newTutorial);
 	}
-		
+
 	public String getName() {
-		return Name;
+		return name;
 	}
+
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
+
 	public String getDisplayName() {
-		return DisplayName;
+		return displayName;
 	}
+
 	public void setDisplayName(String displayName) {
-		DisplayName = displayName;
+		this.displayName = displayName;
 	}
+
 	public String getVersion() {
-		return Version;
+		return version;
 	}
+
 	public void setVersion(String version) {
-		Version = version;
+		this.version = version;
 	}
+
 	public String getEmail() {
-		return Email;
+		return email;
 	}
+
 	public void setEmail(String email) {
-		Email = email;
+		this.email = email;
 	}
+
 	public String getSupportEmail() {
-		return SupportEmail;
+		return supportEmail;
 	}
+
 	public void setSupportEmail(String supportEmail) {
-		SupportEmail = supportEmail;
+		this.supportEmail = supportEmail;
 	}
+
 	public String getTermsOfUseUrl() {
-		return TermsOfUseUrl;
+		return termsOfUseUrl;
 	}
+
 	public void setTermsOfUseUrl(String termsOfUseUrl) {
-		TermsOfUseUrl = termsOfUseUrl;
+		this.termsOfUseUrl = termsOfUseUrl;
 	}
+
 	public String getPrivacyUrl() {
-		return PrivacyUrl;
+		return privacyUrl;
 	}
+
 	public void setPrivacyUrl(String privacyUrl) {
-		PrivacyUrl = privacyUrl;
+		this.privacyUrl = privacyUrl;
 	}
+
 	public String getInstitution() {
-		return Institution;
+		return institution;
 	}
+
 	public void setInstitution(String institution) {
-		Institution = institution;
+		this.institution = institution;
 	}
+
 	public String getInstitutionUrl() {
-		return InstitutionUrl;
+		return institutionUrl;
 	}
+
 	public void setInstitutionUrl(String institutionUrl) {
-		InstitutionUrl = institutionUrl;
+		this.institutionUrl = institutionUrl;
 	}
+
 	public String getInstitutionImageUrl() {
-		return InstitutionImageUrl;
+		return institutionImageUrl;
 	}
+
 	public void setInstitutionImageUrl(String institutionImageUrl) {
-		InstitutionImageUrl = institutionImageUrl;
+		this.institutionImageUrl = institutionImageUrl;
 	}
+
 	public String getMimeType() {
-		return MimeType;
+		return mimeType;
 	}
+
 	public void setMimeType(String mimeType) {
-		MimeType = mimeType;
+		this.mimeType = mimeType;
 	}
+
 	public boolean isSupportsLanguageSyntax() {
-		return SupportsLanguageSyntax;
+		return supportsLanguageSyntax;
 	}
+
 	public void setSupportsLanguageSyntax(boolean supportsLanguageSyntax) {
-		SupportsLanguageSyntax = supportsLanguageSyntax;
+		this.supportsLanguageSyntax = supportsLanguageSyntax;
 	}
+
 	public String getTitle() {
-		return Title;
+		return title;
 	}
+
 	public void setTitle(String title) {
-		Title = title;
+		this.title = title;
 	}
+
 	public String getDescription() {
-		return Description;
+		return description;
 	}
+
 	public void setDescription(String description) {
-		Description = description;
+		this.description = description;
 	}
+
 	public String getQuestion() {
-		return Question;
+		return question;
 	}
+
 	public void setQuestion(String question) {
-		Question = question;
+		this.question = question;
 	}
+
 	public String getUrl() {
-		return Url;
+		return url;
 	}
+
 	public void setUrl(String url) {
-		Url = url;
+		this.url = url;
 	}
+
 	public String getVideoUrl() {
-		return VideoUrl;
+		return videoUrl;
 	}
+
 	public void setVideoUrl(String videoUrl) {
-		VideoUrl = videoUrl;
+		this.videoUrl = videoUrl;
 	}
+
 	public boolean isDisableErrorTable() {
-		return DisableErrorTable;
+		return disableErrorTable;
 	}
+
 	public void setDisableErrorTable(boolean disableErrorTable) {
-		DisableErrorTable = disableErrorTable;
+		this.disableErrorTable = disableErrorTable;
 	}
-	
+
+	public List<Sample> getSamples() {
+		return samples;
+	}
+
+	public void setSamples(List<Sample> samples) {
+		this.samples = samples;
+	}
+
+	public List<Tutorial> getTutorials() {
+		return tutorials;
+	}
+
+	public void setTutorials(List<Tutorial> tutorials) {
+		this.tutorials = tutorials;
+	}
+
 }
